4aed4ab99fd5150b415929317f42c4b1fabefe5a
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-06-06 20:42:09 +0200

    integrate the create method of the radix trie

--- rust-radix-trie/src/lib.rs
+++ rust-radix-trie/src/lib.rs
@@ -1,16 +1,36 @@
-struct RadixTrieNodeChild {
-    next: *const RadixTrieNodeChild,
-    node: *const RadixTrieNode,
-}
+mod rt {
 
-struct RadixTrieNode {
-    key: String,
-    children: *const RadixTrieNodeChild,
+    use std::ptr;
+
+    struct RadixTrieNodeChild {
+        next: *const RadixTrieNodeChild,
+        node: *const RadixTrieNode,
+    }
+
+    pub struct RadixTrieNode {
+        key: String,
+        children: *const RadixTrieNodeChild,
+    }
+
+    impl RadixTrieNode {
+
+        pub fn new(key: &str) -> RadixTrieNode {
+            RadixTrieNode {
+                key: key.to_string(),
+                children: ptr::null(),
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+
+    use rt;
+
     #[test]
-    fn it_works() {
+    fn test_create() {
+
+        let node = rt::RadixTrieNode::new("");
     }
 }
