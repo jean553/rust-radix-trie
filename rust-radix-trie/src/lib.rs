6ef7d67313de46d9ace7d8240f5d60f956dc1719
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-03-14 21:57:31 +0100

    create the radix trie node constructor

--- rust-radix-trie/src/lib.rs
+++ rust-radix-trie/src/lib.rs
@@ -1,7 +1,37 @@
+#[allow(dead_code)]
+
 mod rt {
 
-    struct Node {
-        characters: Vec<char>,
+    /// A radix trie node with a string (array of characters) and children to other nodes.
+    pub struct Node {
+        characters: String,
         children: Vec<Node>,
+    }
+
+    impl Node {
+
+        /// Creates a new radix trie node, with an empty array of characters and an empty list of children nodes.
+        ///
+        /// # Returns:
+        ///
+        /// new radix trie node
+        pub fn new() -> Node {
+            Node {
+                characters: String::new(),
+                children: Vec::new(),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use rt::Node;
+
+    #[test]
+    fn test_node_creation() {
+
+        let node = Node::new();
     }
 }
