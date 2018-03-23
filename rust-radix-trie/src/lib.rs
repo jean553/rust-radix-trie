6581d6a58bda0615811d289dcf64f97e269c6059
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-03-23 13:48:29 +0100

    add a setter for the node characters

--- rust-radix-trie/src/lib.rs
+++ rust-radix-trie/src/lib.rs
@@ -159,6 +159,15 @@ mod rt {
         pub fn get_children(&self) -> &Vec<Node> {
             &self.children
         }
+
+        /// Setter of the node characters.
+        ///
+        /// # Args:
+        ///
+        /// `characters` - the characters to use
+        pub fn set_characters(&mut self, characters: &str) {
+            self.characters = characters.to_string();
+        }
     }
 }
 
